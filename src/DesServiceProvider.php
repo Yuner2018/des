8aae1489f0562fa2bba8dc23d16377c48b4d09de
unknown <chenzyve@nfdaily.com>
2018-12-20 09:28:16 +0800

    first commit

---
+++ src/DesServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Yuner\Des;
+
+use Illuminate\Support\ServiceProvider;
+use Yuner\Aes\Des;
+
+class DesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([__DIR__.'/../config/des.php' => config_path('des.php')]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('des',function(){
+            return new Des();
+        });
+    }
+}
